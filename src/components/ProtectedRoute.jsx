9d3e80d2f7f34e40736e52d0667e2976bdf5346f
Shray <shilpshrayash90@gmail.com>
2025-08-01 20:35:27 +0545

    docs: protected route

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,3 +1,4 @@
+// Protected route component for authentication
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
