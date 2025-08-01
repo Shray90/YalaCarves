b4e5cff2602f9d6b8718a457be379e22c183fc48
Shray <shilpshrayash90@gmail.com>
2025-08-01 20:56:31 +0545

    docs: app jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+// App component JavaScript version
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import { CartProvider } from "./contexts/CartContext";
